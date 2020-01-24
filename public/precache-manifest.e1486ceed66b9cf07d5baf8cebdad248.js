4d9c883455ae3a8757c85a5652438fcf88d8da0c
daravel <natsudragneel736@gmail.com>
2020-01-24 16:10:21 +0700

    First Release

---
+++ public/precache-manifest.e1486ceed66b9cf07d5baf8cebdad248.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52727be4ab49c06baee466d8226c4011",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afa9cdad8476a210957a",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "4bceb8ff92b2234464a0",
+    "url": "/static/js/2.b5a4c341.chunk.js"
+  },
+  {
+    "revision": "786089ef6a78275eeee7a67aba60641a",
+    "url": "/static/js/2.b5a4c341.chunk.js.LICENSE"
+  },
+  {
+    "revision": "afa9cdad8476a210957a",
+    "url": "/static/js/main.03bf4385.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "a718c52d0620222c51bf266faf6e6321",
+    "url": "/static/media/logo2.a718c52d.png"
+  }
+]);
